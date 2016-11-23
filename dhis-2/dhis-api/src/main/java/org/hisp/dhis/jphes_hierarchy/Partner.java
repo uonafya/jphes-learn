8bf2c538d2067c2d0f8f8116209a61617a96f82c
banga <dennisbanga@gmail.com>
2016-11-23 16:24:19 +0300

    created jphes packages

---
+++ dhis-2/dhis-api/src/main/java/org/hisp/dhis/jphes_hierarchy/Partner.java
@@ -0,0 +1,12 @@
+package org.hisp.dhis.jphes_hierarchy;
+
+import org.hisp.dhis.common.BaseNameableObject;
+
+/**
+ * Created by @author bangadennis on 23/11/16.
+ */
+
+public class Partner extends BaseNameableObject
+{
+    private int id;
+}
