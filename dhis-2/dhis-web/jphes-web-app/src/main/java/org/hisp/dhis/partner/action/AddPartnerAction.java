aae854c7ecf1490c366eeff95136032d6344e35d
banga <dennisbanga@gmail.com>
2016-12-14 17:09:27 +0300

    fixes and updates

--- dhis-2/dhis-web/jphes-web-app/src/main/java/org/hisp/dhis/partner/action/AddPartnerAction.java
+++ dhis-2/dhis-web/jphes-web-app/src/main/java/org/hisp/dhis/partner/action/AddPartnerAction.java
@@ -1,8 +1,14 @@
 package org.hisp.dhis.partner.action;
 
+import com.opensymphony.xwork2.Action;
+
 /**
  * Created by afya on 06/12/16.
  */
-public class AddPartnerAction
+public class AddPartnerAction implements Action
 {
+    @Override public String execute() throws Exception
+    {
+        return null;
+    }
 }
